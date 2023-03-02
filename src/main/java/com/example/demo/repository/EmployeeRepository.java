5afcec68ff73993f4dffc046c7ddfcf8d75658cb
zukazuka12 <zurab.vanishvili.1@btu.edu.ge>
2023-03-02 14:31:42 +0400

    Initial commit

---
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import com.example.demo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+
+	List<Employee> findAllByOrderByLastNameAsc();
+	
+}
